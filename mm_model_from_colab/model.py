2025c93c1135b40f39ce7a0c8992a930e87ad31c
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 13:33:41 -0800

    f

--- mm_model_from_colab/model.py
+++ mm_model_from_colab/model.py
@@ -16,8 +16,8 @@ from transformers.modeling_outputs import ModelOutput
 
 
 
-from components import OrpheusProjector
-from config import OrpheusConfig
+from mm_model_from_colab.components import OrpheusProjector
+from mm_model_from_colab.config import OrpheusConfig
 
 @dataclass
 class OrpheusCausalLMOutputWithPast(ModelOutput):
