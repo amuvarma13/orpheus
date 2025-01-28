515a5e6b2d6ea0d62f8cee136cbc9a88f7984a0c
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 15:55:12 -0800

    f

--- mm_model/utils.py
+++ mm_model/utils.py
@@ -1,5 +1,5 @@
 from huggingface_hub import snapshot_download
-
+from concurrent.futures import ThreadPoolExecutor
 
 
 
@@ -32,9 +32,14 @@ class OrpheusUtility():
                 "tokenizer.*"
             ]
         )
+
     def fast_download_from_hub(self, text_model_name="amuvarma/3b-zuckreg-convo", multimodal_model_name="amuvarma/zuck-3bregconvo-automodelcompat"):
-        self._download_from_hub(text_model_name)
-        self._download_from_hub(multimodal_model_name)
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            future_text = executor.submit(self._download_from_hub, text_model_name)
+            future_multimodal = executor.submit(self._download_from_hub, multimodal_model_name)
+            future_text.result()
+            future_multimodal.result()
+        
         print("Downloads complete!")
 
 
