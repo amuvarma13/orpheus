e729377d055dc9cf1c84e3728fb024a5327f62cc
Amartya Varma <amartya.varma@gmail.com>
2025-01-29 18:26:21 -0800

    f

--- mm_model/utils.py
+++ mm_model/utils.py
@@ -121,6 +121,7 @@ class OrpheusUtility():
         self._initialise_model()
 
         audio_features = speech.to(dtype=torch.bfloat16).to("cuda")
+        audio_features = audio_features.unsqueeze(0)
         audio_embeds = self.model.multi_modal_projector(audio_features)
         start_token = torch.tensor([[self.special_tokens["start_of_human"]]], dtype=torch.int64)
         end_tokens = torch.tensor([[self.special_tokens["end_of_text"], self.special_tokens["end_of_human"], self.special_tokens["start_of_ai"]]], dtype=torch.int64)
