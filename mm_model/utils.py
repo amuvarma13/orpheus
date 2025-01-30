0a8a7ae89c162378c4e6a39869fd3e6793650154
Amartya Varma <amartya.varma@gmail.com>
2025-01-30 10:51:58 -0800

    f

--- mm_model/utils.py
+++ mm_model/utils.py
@@ -56,7 +56,7 @@ class OrpheusConversation():
 
         text_embeds = text_embeds.to(dtype=torch.bfloat16).to(self.model.device)
         start_token = torch.tensor([[128259]], dtype=torch.int64)
-        end_tokens = torch.tensor([[128009, 128260, 128261]], dtype=torch.int64)
+        end_tokens = torch.tensor([[128009, 128260]], dtype=torch.int64)
 
         start_token = start_token.to(self.model.device)
         end_tokens = end_tokens.to(self.model.device)
@@ -126,7 +126,7 @@ class OrpheusConversation():
 
         print("successffully updated existing embeds")
 
-        print(output_tokens)
+        print(output_tokens.shape)
 
         output = self.parent.parse_output_tokens(output_tokens)
         return output
