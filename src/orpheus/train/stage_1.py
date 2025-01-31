e9665e47dfcf4c1f135919e0afc7decf8817abc1
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 11:40:07 -0800

    pypi 0.0.12

--- src/orpheus/train/stage_1.py
+++ src/orpheus/train/stage_1.py
@@ -60,8 +60,9 @@ class Stage_1_Trainer():
 
         self.snac_model = SNAC.from_pretrained("hubertsiuzdak/snac_24khz")
 
-        self.processed_text_dataset = self._process_text_dataset(self.text_dataset)
         self.processed_speech_dataset = self._process_speech_dataset(self.speech_dataset)
+        self.processed_text_dataset = self._process_text_dataset(self.text_dataset)
+        
         self.dataset = BatchedAlternatingDataset(self.processed_text_dataset, speech_dataset, batch_total=self.batch_size*self.num_gpus)
 
 
