e5719c030fcf0d150c8057d5fc77ca7ff633d44a
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 23:05:25 -0800

    pypi 0.0.12

--- src/orpheus/train/stage_5.py
+++ src/orpheus/train/stage_5.py
@@ -39,12 +39,13 @@ class AudioChatDataCollator:
             [start_token, user_input_ids, end_tokens], dim=1)
         snac_tokens_tensor = torch.tensor([snac_tokens], dtype=torch.int64)
 
-        if len(snac_tokens) > 0:
-            labels = torch.cat([start_token, user_input_ids, end_tokens,
-                            assistant_input_ids, final_tokens, snac_tokens_tensor, post_assistant_tokens], dim=1)
+        # if len(snac_tokens) > 0:
+
+        #     labels = torch.cat([start_token, user_input_ids, end_tokens,
+        #                     assistant_input_ids, final_tokens, snac_tokens_tensor, post_assistant_tokens], dim=1)
             
-        else:
-            labels = torch.cat([start_token, user_input_ids, end_tokens,
+        # else:
+        labels = torch.cat([start_token, user_input_ids, end_tokens,
                             assistant_input_ids, final_tokens], dim=1)
 
         true_labels = torch.full_like(labels, -100)
