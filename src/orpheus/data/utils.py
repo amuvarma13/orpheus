4aedcefcb8ac8586c75e22494ccf2b533012520f
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 21:21:31 -0800

    pypi 0.0.12

--- src/orpheus/data/utils.py
+++ src/orpheus/data/utils.py
@@ -9,12 +9,9 @@ import torchaudio.transforms as T
 class OrpheusDataProcessor():
     def __init__(self):
         self.voices = ["af_alloy", "af_aoede", "af_bella", "af_heart", "af_jessica", "af_kore", "af_nicole", "af_nova", "af_river", "af_sarah", "af_sky", "am_adam", "am_echo", "am_eric", "am_fenrir", "am_liam", "am_michael", "am_onyx", "am_puck", "am_santa", "bf_alice", "bf_emma", "bf_isabella", "bf_lily", "bm_daniel", "bm_fable", "bm_george", "bm_lewis"]
-        self.pipeline = KPipeline(lang_code='b') 
+        self.pipeline = KPipeline(lang_code='b', device="cuda") 
         self.resampler = T.Resample(orig_freq=24000, new_freq=16000)
-
-        
         pass
-
 
     def fast_load_dataset(self, dataset_name, split="train"):
         dataset = self._load_dataset(dataset_name, split=split)
@@ -39,10 +36,6 @@ class OrpheusDataProcessor():
 
         dataset = dataset.map(add_audio_fn, batched=False)
         return dataset
-
-
-
-
 
     def _add_audio(self, example, column_name, audio_column_name, target_sr=16000):
         try:
