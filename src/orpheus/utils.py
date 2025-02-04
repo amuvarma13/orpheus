ec07075d5f7c3959e4eb52b6984027a023669590
Amartya Varma <amartya.varma@gmail.com>
2025-02-04 14:33:21 -0800

    f

--- src/orpheus/utils.py
+++ src/orpheus/utils.py
@@ -96,10 +96,13 @@ class OrpheusConversation():
         end_embeds = self.model.get_input_embeddings()(end_tokens)
         start_embeds = start_embeds.to(dtype=torch.bfloat16)
         end_embeds = end_embeds.to(dtype=torch.bfloat16)
+        existing_embeds =existing_embeds.to("cpu")
         if self.existing_embeds is not None:
             all_embeds = torch.cat([self.existing_embeds, start_embeds, audio_embeds, end_embeds], dim=1)
         else:
             all_embeds = torch.cat([start_embeds, audio_embeds, end_embeds], dim=1)
+
+        all_embeds = all_embeds.to(self.model.device).to(dtype=torch.bfloat16)
 
         return all_embeds
     
