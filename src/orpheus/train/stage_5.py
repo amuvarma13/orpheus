a92542f527b2ba61e207103599bc266a1da8e800
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 19:54:24 -0800

    f

--- src/orpheus/train/stage_5.py
+++ src/orpheus/train/stage_5.py
@@ -215,7 +215,7 @@ class Stage_5_Trainer():
     
     def _add_codes_to_dataset(self, dataset):
         self.sr = dataset[0]["answer_audio"]["sampling_rate"]
-        dataset = dataset.map(self._add_codes)
+        dataset = dataset.map(self._add_codes, cache_file_name=None)
         return dataset
     
 
